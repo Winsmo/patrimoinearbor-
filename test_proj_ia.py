46ccf68ce55a1b902b747448484eb0f3fcdee6b7
julesyann <167093194+AZazerty12@users.noreply.github.com>
2024-06-10 14:03:37 +0200

    Update test_proj_ia.py

--- test_proj_ia.py
+++ test_proj_ia.py
@@ -1,3 +1,13 @@
 print("hello")
 print("world")
 print("coucou")
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+import pandas as pd
+
+# Chargement des données
+data = pd.read_csv("C:/Users/jeanj/OneDrive/Bureau/projet_bid_data/Patrimoine_Arboré_(RO).csv")
+
+
+
+# Sélection des colonnes pertinentes
+data1 = data['X', 'Y', 'haut_tot', 'feuillage']
