c74ddf176a686867cbcfb0f240122695a26db077
Winsmo <enzo.laporte@icloud.com>
2024-06-11 10:12:08 +0200

    DBSCAN Clustering

--- test_proj_ia.py
+++ test_proj_ia.py
@@ -12,17 +12,17 @@ data1 = data[['haut_tot','tronc_diam']].copy()
 # Vérification des types de données
 print(data1.dtypes)
 
-# Choix du nombre de clusters
-agglo = AgglomerativeClustering(n_clusters=3)
+# Choix des paramètres
+dbscan = DBSCAN(eps=0.1, min_samples=5)
 
 # Appliquer le clustering
-data1['cluster'] = agglo.fit_predict(data1[['haut_tot','tronc_diam']])
+data1['cluster'] = dbscan.fit_predict(data1)
 
 # Affichage des résultats
 plt.scatter(data1['haut_tot'], data1['tronc_diam'], c=data1['cluster'], cmap='viridis')
 plt.xlabel('haut_tot')
 plt.ylabel('tronc_diam')
-plt.title('Agglomerative Clustering')
+plt.title('DBSCAN Clustering')
 plt.show()
 
 
