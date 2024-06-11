10117de08b43b1e34cbff9046e5a05ae787f608f
Winsmo <enzo.laporte@icloud.com>
2024-06-11 10:08:33 +0200

    agglomerative clustering

--- test_proj_ia.py
+++ test_proj_ia.py
@@ -1,13 +1,28 @@
-print("hello")
-print("world")
-print("coucou")
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.cluster import AgglomerativeClustering
+from sklearn.cluster import DBSCAN
 
 # Chargement des données
-data = pd.read_csv("C:/Users/jeanj/OneDrive/Bureau/projet_bid_data/Patrimoine_Arboré_(RO).csv")
+data = pd.read_csv("./Data_Arbre.csv")
+
+# Sélection des colonnes pertinentes et création d'une copie explicite
+data1 = data[['haut_tot','tronc_diam']].copy()
+
+# Vérification des types de données
+print(data1.dtypes)
+
+# Choix du nombre de clusters
+agglo = AgglomerativeClustering(n_clusters=3)
+
+# Appliquer le clustering
+data1['cluster'] = agglo.fit_predict(data1[['haut_tot','tronc_diam']])
+
+# Affichage des résultats
+plt.scatter(data1['haut_tot'], data1['tronc_diam'], c=data1['cluster'], cmap='viridis')
+plt.xlabel('haut_tot')
+plt.ylabel('tronc_diam')
+plt.title('Agglomerative Clustering')
+plt.show()
 
 
-
-# Sélection des colonnes pertinentes
-data1 = data['X', 'Y', 'haut_tot', 'feuillage']
