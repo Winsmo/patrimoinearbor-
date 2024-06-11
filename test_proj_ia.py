ad3d71d13b0fcdf8cedd62a362d5a5f21f8f1841
Winsmo <enzo.laporte@icloud.com>
2024-06-11 10:55:34 +0200

    K-means Clustering

--- test_proj_ia.py
+++ test_proj_ia.py
@@ -12,17 +12,28 @@ data1 = data[['haut_tot','tronc_diam']].copy()
 # Vérification des types de données
 print(data1.dtypes)
 
-# Choix des paramètres
-dbscan = DBSCAN(eps=0.1, min_samples=5)
+import pandas as pd
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+
+# Charger les données
+data = pd.read_csv("./Data_Arbre.csv")
+
+# Sélection des colonnes pertinentes
+data1 = data[['tronc_diam', 'haut_tot']]
+
+# Choix du nombre de clusters
+kmeans = KMeans(n_clusters=3, random_state=42)
 
 # Appliquer le clustering
-data1['cluster'] = dbscan.fit_predict(data1)
+data1['cluster'] = kmeans.fit_predict(data1)
 
 # Affichage des résultats
 plt.scatter(data1['haut_tot'], data1['tronc_diam'], c=data1['cluster'], cmap='viridis')
 plt.xlabel('haut_tot')
 plt.ylabel('tronc_diam')
-plt.title('DBSCAN Clustering')
+plt.title('K-Means Clustering')
 plt.show()
+
 
 
