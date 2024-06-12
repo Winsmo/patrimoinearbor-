1fbff8d5d2b32e6c9626d8de5c199fa6c2e9e036
Winsmo <enzo.laporte@icloud.com>
2024-06-12 10:05:14 +0200

    3 estimations clusters

--- test_proj_ia.py
+++ test_proj_ia.py
@@ -1,24 +1,35 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.cluster import KMeans, AgglomerativeClustering, DBSCAN
-from sklearn.metrics import silhouette_score
+from sklearn.cluster import KMeans
+from sklearn.metrics import davies_bouldin_score, silhouette_score
 
-# Charger les données
+# Chargement des données
 data = pd.read_csv("./Data_Arbre.csv")
 
-# Sélection des colonnes pertinentes
-data1 = data[['haut_tot', 'tronc_diam']].copy()
+# Sélection des colonnes pertinentes et création d'une copie explicite
+data1 = data[['haut_tot', 'longitude', 'latitude']].copy()
 
 # Vérification des types de données
 print(data1.dtypes)
 
-# Méthode du coude pour déterminer le nombre optimal de clusters
+# Méthode du coude (Elbow Method) et Davies-Bouldin Index
 inertia = []
-K = range(1, 11)
+db_scores = []
+silhouette_scores = []
+K = range(2, 10)  # Davies-Bouldin index et silhouette score ne sont pas définis pour k=1
 for k in K:
     kmeans = KMeans(n_clusters=k, random_state=42)
     kmeans.fit(data1)
     inertia.append(kmeans.inertia_)
+    
+    # Calculer l'indice de Davies-Bouldin pour chaque K
+    labels = kmeans.labels_
+    db_score = davies_bouldin_score(data1, labels)
+    db_scores.append(db_score)
+    
+    # Calculer le coefficient de silhouette pour chaque K
+    silhouette_avg = silhouette_score(data1, labels)
+    silhouette_scores.append(silhouette_avg)
 
 # Tracer la courbe d'inertie
 plt.figure(figsize=(8, 5))
@@ -28,41 +39,61 @@ plt.ylabel('Inertie')
 plt.title('Méthode du coude pour déterminer le nombre optimal de clusters')
 plt.show()
 
-# Calculer le score de silhouette pour différents nombres de clusters
-silhouette_scores = []
-K = range(2, 11)
-for k in K:
-    kmeans = KMeans(n_clusters=k, random_state=42)
-    cluster_labels = kmeans.fit_predict(data1)
-    silhouette_avg = silhouette_score(data1, cluster_labels)
-    silhouette_scores.append(silhouette_avg)
+# Tracer la courbe de l'indice de Davies-Bouldin
+plt.figure(figsize=(8, 5))
+plt.plot(K, db_scores, 'bo-')
+plt.xlabel('Nombre de clusters K')
+plt.ylabel('Indice de Davies-Bouldin')
+plt.title('Indice de Davies-Bouldin pour déterminer le nombre optimal de clusters')
+plt.show()
 
-# Tracer le score de silhouette
+# Tracer la courbe du coefficient de silhouette
 plt.figure(figsize=(8, 5))
 plt.plot(K, silhouette_scores, 'bo-')
 plt.xlabel('Nombre de clusters K')
-plt.ylabel('Score de silhouette')
-plt.title('Score de silhouette pour déterminer le nombre optimal de clusters')
+plt.ylabel('Coefficient de silhouette')
+plt.title('Coefficient de silhouette pour déterminer le nombre optimal de clusters')
 plt.show()
 
-# Agglomerative Clustering
-agglo = AgglomerativeClustering(n_clusters=3)
-data1['cluster_agglo'] = agglo.fit_predict(data1[['haut_tot', 'tronc_diam']])
-plt.figure(figsize=(8, 5))
-plt.scatter(data1['haut_tot'], data1['tronc_diam'], c=data1['cluster_agglo'], cmap='viridis')
-plt.xlabel('haut_tot')
-plt.ylabel('tronc_diam')
-plt.title('Agglomerative Clustering')
-plt.show()
+# Choisir le nombre optimal de clusters
+optimal_k = K[silhouette_scores.index(max(silhouette_scores))]
+kmeans = KMeans(n_clusters=optimal_k, random_state=42)
+clusters = kmeans.fit_predict(data1)
 
-# DBSCAN
-dbscan = DBSCAN(eps=1, min_samples=5)
-data1['cluster_dbscan'] = dbscan.fit_predict(data1[['haut_tot', 'tronc_diam']])
-plt.figure(figsize=(8, 5))
-plt.scatter(data1['haut_tot'], data1['tronc_diam'], c=data1['cluster_dbscan'], cmap='viridis')
-plt.xlabel('haut_tot')
-plt.ylabel('tronc_diam')
-plt.title('DBSCAN Clustering')
-plt.show()
+# Ajouter les clusters aux données
+data1['cluster'] = clusters
+
+print(f"Le nombre optimal de clusters est : {optimal_k}")
 
 
+############################################################################
+#
+## Score de silhouette (Silhouette Score)
+#silhouette_scores = []
+#K = range(2, 11)
+#for k in K:
+#    kmeans = KMeans(n_clusters=k, random_state=42)
+#    cluster_labels = kmeans.fit_predict(data1)
+#    silhouette_avg = silhouette_score(data1, cluster_labels)
+#    silhouette_scores.append(silhouette_avg)
+#
+## Tracer le score de silhouette
+#plt.figure(figsize=(8, 5))
+#plt.plot(K, silhouette_scores, 'bo-')
+#plt.xlabel('Nombre de clusters K')
+#plt.ylabel('Score de silhouette')
+#plt.title('Score de silhouette pour déterminer le nombre optimal de clusters')
+#plt.show()
+#
+## Choix du nombre de clusters basé sur les résultats précédents, par exemple 3
+#optimal_k = 3
+#kmeans = KMeans(n_clusters=optimal_k, random_state=42)
+#data1['cluster'] = kmeans.fit_predict(data1)
+#
+## Affichage des résultats
+#plt.scatter(data1['haut_tot'], c=data1['cluster'], cmap='viridis')
+#plt.xlabel('Hauteur totale (haut_tot)')
+#plt.ylabel('?')
+#plt.title('K-Means Clustering avec K optimal')
+#plt.show()
+#
