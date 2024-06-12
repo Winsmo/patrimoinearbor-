1160a3ae7d06e20e4ade409ec162772bf882130d
Winsmo <enzo.laporte@icloud.com>
2024-06-12 15:02:42 +0200

    Carte Ok+ parce que ca marche avec ce que rentre l'utilisateur mtn

--- test_proj_ia.py
+++ test_proj_ia.py
@@ -10,7 +10,7 @@ def courbe_inertie(K, inertias):
     plt.plot(K, inertias, 'bo-')
     plt.xlabel('Nombre de clusters K')
     plt.ylabel('Inertie')
-    plt.title('Méthode du coude pour déterminer le nombre optimal de clusters')
+    plt.title('Methode du coude pour déterminer le nombre optimal de clusters')
     plt.show()
 
 def indice_davies_bouldin(K, db_scores):
@@ -39,11 +39,12 @@ def main():
     # Vérification des types de données
     print(data1.dtypes)
 
+
     # Méthode du coude et Davies-Bouldin Index
     inertie = []
     db_scores = []
     silhouette_scores = []
-    K = range(2, 10)  # Davies-Bouldin et silhouette ne sont pas définis pour k=1
+    K = range(2, 6)  # Davies-Bouldin et silhouette ne sont pas définis pour k=1
     for k in K:
         kmeans = KMeans(n_clusters=k, random_state=42)
         kmeans.fit(data1)
@@ -54,34 +55,49 @@ def main():
         silhouette_avg = silhouette_score(data1, labels)
         silhouette_scores.append(silhouette_avg)
 
+    # Pour la méthode du coude
+    optimal_k_elbow = K[inertie.index(max(inertie))]
+    print(f"Le nombre optimal de clusters pour la methode du coude est : {optimal_k_elbow}")
+
+    # Pour l'indice de Davies-Bouldin
+    optimal_k_db = K[db_scores.index(min(db_scores))]
+    print(f"Le nombre optimal de clusters pour l'indice de Davies-Bouldin est : {optimal_k_db}")
+
+    # Pour le coefficient de silhouette
+    optimal_k_silhouette = K[silhouette_scores.index(max(silhouette_scores))]
+    print(f"Le nombre optimal de clusters pour le coefficient de silhouette est : {optimal_k_silhouette}")
+
     # Appel des fonctions pour tracer les courbes
     courbe_inertie(K, inertie)
     indice_davies_bouldin(K, db_scores)
     coef_silhouette(K, silhouette_scores)
 
+
     # Demander à l'utilisateur de rentrer le nombre de clusters
-    nb_clusters = 18
+    nb_clusters = int(input("Nombre de clusters voulu :"))
+
 
     # Appliquer le KMeans avec le nombre de clusters choisi par l'utilisateur
     kmeans = KMeans(n_clusters=nb_clusters, random_state=42)
     clusters = kmeans.fit_predict(data1)
-    print(f'clusters après clusters = kmeans.fit_predict(data1)')
-    print(clusters)
+
     # Ajouter les clusters aux données
     data1['cluster'] = pd.Series(clusters, name='cluster')
 
     # Visualisation des clusters sur une carte avec Plotly
     fig = px.scatter_mapbox(data1, 
-                            lat="latitude", 
-                            lon="longitude", 
-                            color="cluster", 
-                            size="haut_tot",
-                            color_continuous_scale=px.colors.qualitative.Set1,
-                            size_max=15, 
-                            zoom=10,
-                            mapbox_style="carto-positron",
-                            title="Visualisation des arbres par clusters")
+        lat="latitude", 
+        lon="longitude", 
+        color="cluster", 
+        size="haut_tot",
+        color_continuous_scale=px.colors.qualitative.Set1,
+        size_max=15, 
+        zoom=10,
+        mapbox_style="carto-positron",
+        title="Visualisation des arbres par clusters")
+    
     fig.show()
+    
 main()
 
 
